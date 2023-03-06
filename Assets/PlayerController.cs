41063cb93b82328e8cb91ea7073c97199858c4cc
aa <a@a>
2023-03-06 15:21:22 +0100

    hh

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,10 +7,16 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     Vector2 movementVector;
+    Transform bulletSpawn;
+    public float bulletSpeed = 20f;
+    public GameObject bulletPrefab;
+    public float PlayerSpeed = 2;
     // Start is called before the first frame update
     void Start()
     {
         movementVector = Vector2.zero;
+        bulletSpawn = transform.Find("gun");
+
     }
 
     // Update is called once per frame
@@ -20,13 +26,22 @@ public class PlayerController : MonoBehaviour
         // transform.position = new Vector3(transform.position.x + movementVector.x * Time.deltaTime,
         //    0,
         //  transform.position.z + movementVector.y *Time.deltaTime);
-        transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime);
+        transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime * PlayerSpeed);
     }
 
     void OnMove(InputValue inputValue)
     {
         movementVector = inputValue.Get<Vector2>();
         Debug.Log(movementVector.ToString());
+
+    }
+    void OnFire()
+    {
+
+        GameObject Bullet = Instantiate(bulletPrefab, bulletSpawn );
+        Bullet.transform.parent = null;
+        Bullet.GetComponent<Rigidbody>().AddForce(Bullet.transform.forward * bulletSpeed, ForceMode.VelocityChange);
+        Destroy(Bullet, 10);
 
     }
 }
---
+++ Assets/zombieScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieScript : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Player.transform.position);
+        //Vector3 playerDirection = transform.position - Player.transform.position;
+
+        transform.Translate(transform.forward * Time.deltaTime);
+    }
+}
